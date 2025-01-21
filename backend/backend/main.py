12e4c104c52442a3105ff4ef91f8293c86580952
Emmanuel Guefif <eguefif@fastmail.com>
2025-01-20 20:57:52 -0500

    Add enpoint for get user and all user

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,7 +1,9 @@
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 
+from sqlmodel import select
 from sql_engine import create_db_and_tables, SessionDep
 from account.model import Account
 
@@ -39,3 +41,22 @@ async def create_account(account: Account, session: SessionDep):
     session.commit()
     session.refresh(account)
     return account
+
+
+@app.get("/user/")
+async def all_user(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Account]:
+    accounts = session.exec(select(Account).offset(offset).limit(limit)).all()
+    return accounts
+
+
+@app.get("/user/")
+async def get_user(username: str, session: SessionDep) -> Account:
+    user = session.get(Account, username)
+    if not user:
+        raise HTTPException(status_code=404, detail="User notfound")
+
+    return user
