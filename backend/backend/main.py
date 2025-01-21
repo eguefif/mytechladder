c564b537cdf8ddf12536183b8d6f9fd407d78aa7
Emmanuel Guefif <eguefif@fastmail.com>
2025-01-21 07:32:22 -0500

    Add login endpoint

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,13 +1,16 @@
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated
 
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from sqlmodel import select
 from sql_engine import create_db_and_tables, SessionDep
 from account.model import Account
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.on_event("startup")
@@ -34,6 +37,22 @@ def read_root():
     return {"message": "Hello, World"}
 
 
+@app.post("/token")
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: SessionDep
+):
+    statement = select(Account).where(Account.username == form_data.username)
+    user_dict = session.exec(statement).first()
+    print(user_dict, form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user_password = user_dict.password
+    print(user_password, " ", form_data.password)
+    if not user_password == form_data.password:
+        raise HTTPException(status_code=400, detail="Inccord username or password")
+    return {"access_token": user_dict.username, "token_type": "bearer"}
+
+
 @app.post("/create_account/")
 async def create_account(account: Account, session: SessionDep):
     print(account)
@@ -45,6 +64,7 @@ async def create_account(account: Account, session: SessionDep):
 
 @app.get("/user/")
 async def all_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
     session: SessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
